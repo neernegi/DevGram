ab235614775930d3656b965a607ea68bb77de9b2
Neeraj <neerajnegi3443@gmail.com>
2023-12-08 16:22:35 +0530

    ch

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,16 +1,16 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, Navigate, Link } from "react-router-dom";
 
 const AuthLayout = () => {
   const isAuthenticated = false;
   return (
     <>
       {isAuthenticated ? (
-        <section className="flex flex-1 justify-center items-center flex-col py-10">
-          <Outlet />
-        </section>
+        <Link to={"/sign-in"} />
       ) : (
         <>
-          <Navigate to={"/sign-in"} />
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
         </>
       )}
     </>
